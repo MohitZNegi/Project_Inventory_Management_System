ef59e2208b672505f696160c6be4b939e9cc7d15
CameronD <116122766+CamDal@users.noreply.github.com>
2024-05-07 16:59:11 +1200

    Navbar UI
    Updates to the navbar.

--- Inventory_Management_System_Application/Inventory_Management_System/obj/Debug/net6.0/Inventory_Management_System.AssemblyInfo.cs
+++ Inventory_Management_System_Application/Inventory_Management_System/obj/Debug/net6.0/Inventory_Management_System.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventory_Management_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cda53c6be991bf7b4b1cd00b9fda39186a2e2fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9e2734e9214286f1510a3dfd1547e4516d6a577")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventory_Management_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventory_Management_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
