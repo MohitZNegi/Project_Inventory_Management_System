876ba274830af35c554ec2ae4e35eea9acbf5826
MohitZNegi <mohit26negi@gmail.com>
2024-06-03 20:06:05 +1200

    final commit

--- Inventory_Management_System_Application/Inventory_Management_System/obj/Debug/net6.0/Inventory_Management_System.AssemblyInfo.cs
+++ Inventory_Management_System_Application/Inventory_Management_System/obj/Debug/net6.0/Inventory_Management_System.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventory_Management_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d1c88f621f9ea7bcc3c5cf1e845155aeeb879f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventory_Management_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventory_Management_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
