9d1c88f621f9ea7bcc3c5cf1e845155aeeb879f8
CameronD <116122766+CamDal@users.noreply.github.com>
2024-06-03 14:44:00 +1200

    Final Changes
    Adding Final Changes to Code

--- Inventory_Management_System_Application/Inventory_Management_System/obj/Debug/net6.0/Inventory_Management_System.AssemblyInfo.cs
+++ Inventory_Management_System_Application/Inventory_Management_System/obj/Debug/net6.0/Inventory_Management_System.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventory_Management_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b14a53d240d5b1bacf9be5744f62f66e9658f51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d78629927a01c3f9a24c84758d31377142a15898")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventory_Management_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventory_Management_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
